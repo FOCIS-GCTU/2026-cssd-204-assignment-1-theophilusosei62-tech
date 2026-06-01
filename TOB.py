8224e7582641ca3cf99050e2ae76816830e989f2
theophilusosei62-tech <theophilusosei62@gmail.com>
2026-06-01 15:53:06 -0400

    Add files via upload

---
+++ TOB.py
@@ -0,0 +1,34 @@
+# File: TOB.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+# Name: Theophilus Osei Bonsu
+# STUDENT ID: 2425403196
+# Email: 2425403196@live.gctu.edu.gh
+# Grader: Mr Augustus Buckman
+# On my honor, Theophilus Osei , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+def main():
+    # Print my initials in large letters
+
+    print()
+    print("...TOB")
+    print()
+
+    print("...TTTTTTTTTTTT...OOOOOOOOOOOO...BBBBBBBBBBBB...****")
+    print("......TTTTTT......OO......OO....BB......BB....****")
+    print("......TTTTTT......OO......OO....BB......BB....****")
+    print("......TTTTTT......OO......OO....BBBBBBBBBBBB...")
+    print("......TTTTTT......OO......OO....BB......BB....")
+    print("......TTTTTT......OO......OO....BB......BB....")
+    print("......TTTTTT......OO......OO....BBBBBBBBBBBB...")
+    print("......TTTTTT......OO......OO....BB......BB....")
+    print("......TTTTTT......OO......OO....BB......BB....")
+    print("......TTTTTT......OOOOOOOOOOOO...BBBBBBBBBBBB...****")
+
+    print()
+
+
+main()
